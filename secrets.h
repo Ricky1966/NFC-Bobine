22346423e78a046ef126b90cd801d2e1d08afa99
Ricky1966 <riccardo.bramati@gmail.com>
2023-08-10 22:27:29 +0200

    Update secrets.h

--- secrets.h
+++ secrets.h
@@ -3,7 +3,7 @@
 #ifndef _SECTRETS_H
 #define _SECTRETS_H
 
-const char* host = "esp32";
+const char* host = "hostname";
 const char* ssid = "your ssid";
 const char* password = "your pwd";
 const char* mqtt_server = "broker address";
