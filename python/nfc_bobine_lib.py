5728c2c942dc6b4edc5833b24370efba9ef9055d
dinamitebla <francesco.palazzi2004@gmail.com>
2023-08-26 21:50:04 +0200

    modifica codice thumbnail

--- python/nfc_bobine_lib.py
+++ python/nfc_bobine_lib.py
@@ -46,9 +46,9 @@ def extracting_data(lines):
             print("filament_used_mm :",info )
             data['filament_lenght'] = info
         elif (line.find("Filament used:")>0):
-            info = line.split(': ')[-1][:-1]
-            print("filament_used_mm :",info*1000 )
-            data['filament_lenght'] = info*1000
+            info = line.split(': ')[-1][:-2]
+            print("filament_used_mm :",float(info)*1000 )
+            data['filament_lenght'] = float(info)*1000
 
         elif (line.find("total filament used [g] = ")>0) :
             #; filament used [g] = 1.50
@@ -77,79 +77,58 @@ def extracting_data(lines):
             data['first_layer_temperature'] = info
     return data
 
-def thumbarnail_small(file_choiced):
-    print(file_choiced)
-    file_choiced1 = open(file_choiced, "r")
-    lines = file_choiced1.readlines()
-    x = len(lines)
-    print("thumbarnail function")
-    read_line = False
-    img_file_choiced = open('temp1.txt', 'w')
-    count = 0
-    for line in lines :
-        if (line.find("begin 32x32")>0 ) : # 1108 is numbers of chars per image 400x300 
-            print("thumbnail begin 32x32 1108")
-            print(line)
-            read_line = True
-        elif (line.find("thumbnail e")>0 and read_line == True) : 
-            print("thumbnail end")
-            print(line)
-            read_line = False
-            img_file_choiced.close()
-            break
-        else :
-            if read_line == True :
-                dec_tmp = line.replace("; ", "")
-                img_file_choiced.write(dec_tmp)
-                #print(dec_tmp)
-    img_file_choiced.close()
-    file = open('temp1.txt', 'rb')
-    encoded_data = file.read()
-    file.close()
-    #decode base64 string data
-    decoded_data=base64.b64decode((encoded_data))
-    img_file = open('small_preview.png', 'wb')
-    img_file.write(decoded_data)
-    img_file.close()
-    os.remove('temp1.txt')
-    return
+def thumbnail_small(lines, slicer):
+    if slicer == 'Prusa':
+        print("thumbnail small function")
+        read_line = False
+        encoded_data = ''
+        for line in lines:
+            if line.find("begin 32x32")>0: # 1108 is numbers of chars per image 400x300 
+                print("thumbnail begin 32x32 1108")
+                print(line)
+                read_line = True
+            elif line.find("thumbnail e")>0 and read_line == True: 
+                print("thumbnail end")
+                print(line)
+                read_line = False
+                break
+            else :
+                if read_line == True :
+                    encoded_data = encoded_data + line.replace("; ", "")
+        #decode base64 string data
+        decoded_data=base64.b64decode(encoded_data)
+        img_file = open('small_preview.png', 'wb')
+        img_file.write(decoded_data)
+        img_file.close()
+    elif os.path.isfile('small_preview.png'):
+        os.remove('small_preview.png')
 
-def thumbarnail_large(file_choiced):
-    print(file_choiced)
-    file_choiced1 = open(file_choiced, "r")
-    lines = file_choiced1.readlines()
-    x = len(lines)
-    print("thumbarnail function")
-    read_line = False
-    img_file_choiced = open('temp.txt', 'w')
-    count = 0
-    for line in lines :
-        if (line.find("begin 400x300")>0 ) : # 21284 is numbers of chars per image 400x300 
-            print("thumbnail begin 400x300 21284")
-            print(line)
-            read_line = True
-        elif (line.find("thumbnail e")>0 and read_line == True) : 
-            print("thumbnail end")
-            print(line)
-            read_line = False
-            img_file_choiced.close()
-            break
-        else :
-            if read_line == True :
-                dec_tmp = line.replace("; ", "")
-                img_file_choiced.write(dec_tmp)
-                #print(dec_tmp)
-    img_file_choiced.close()
-    file = open('temp.txt', 'rb')
-    encoded_data = file.read()
-    file.close()
-    #decode base64 string data
-    decoded_data=base64.b64decode((encoded_data))
-    img_file = open('preview.png', 'wb')
-    img_file.write(decoded_data)
-    img_file.close()
-    os.remove("temp.txt")
-    return
+def thumbnail_large(lines, slicer):
+    if slicer == 'Prusa' or slicer == 'Orca':
+        print("thumbnail function")
+        read_line = False
+        encoded_data = ''
+        for line in lines :
+            if line.find("begin 400x300")>0 or line.find("begin 300x300")>0: # 21284 is numbers of chars per image 400x300 
+                #the previous or is a temporary fix 
+                print("thumbnail begin 400x300 21284")
+                print(line)
+                read_line = True
+            elif (line.find("thumbnail e")>0 and read_line == True) : 
+                print("thumbnail end")
+                print(line)
+                read_line = False
+                break
+            else :
+                if read_line == True :
+                    encoded_data = encoded_data + line.replace("; ", "")
+        #decode base64 string data
+        decoded_data=base64.b64decode(encoded_data)
+        img_file = open('preview.png', 'wb')
+        img_file.write(decoded_data)
+        img_file.close()
+    elif os.path.isfile('preview.png'):
+        os.remove('preview.png')
 
 def uid_bobine():
     pass
